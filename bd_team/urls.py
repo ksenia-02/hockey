3de5acc55fe2bd2f6d17bbf34abc406de07ccd15
Ksenia <ksenksenia02@gmail.com>
2021-12-24 14:46:36 +0300

    Main menu; player | player_list

--- bd_team/urls.py
+++ bd_team/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', main_page, name = 'main_page'),
-    path('add_player/', add_page_player, name = 'add_player'),
+    path('addplayer/', add_page_player, name = 'add_player'),
+    path('listplayer/',  post_list_players, name = 'list_players'),
 ]
--- bd_team/views.py
+++ bd_team/views.py
@@ -3,11 +3,12 @@ from .models import *
 from .forms import *
 
 #menu = ['Расписание игр', 'Карточка игрока', 'Рейтинг игроков']
-menu = [{'title': "Добавить игрока", 'url_name': 'add_player'}
+menu = [{'title': "Добавить игрока", 'url_name': 'add_player'},
+        {'title': "Игроки", 'url_name': 'list_players'},
 ]
 
 def main_page(request):
-   return render(request, 'bd_team/base.html', {'menu':menu})
+   return render(request, 'bd_team/base.html', {'menu':menu, 'title':'Главная'})
 
 
 def add_page_player(request):
@@ -20,8 +21,18 @@ def add_page_player(request):
    else:
       form = AddPlayer()
    context = {
+      'title':'Добавление игрока',
       'menu': menu,
       'form': form,
    }
    return render(request, 'bd_team/add_player.html', context)
 
+
+def post_list_players(request):
+   players = Player.objects.all()
+   context = {
+      'title':'Игроки',
+      'menu': menu,
+      'players': players,
+   }
+   return render(request, 'bd_team/players_list.html', context)
